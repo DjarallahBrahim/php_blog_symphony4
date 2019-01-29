41fb41fcd0015911dc849ed6dd2e28557aa2d3be
achrafsmlali <smlali.achraf@gmail.com>
2019-01-29 17:09:56 +0100

    [feat]add authentication

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request, AuthenticationUtils $authUtils)
+    {
+        $error = $authUtils->getLastAuthenticationError();
+
+        $lastUserName = $authUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'error'=> $error,
+            'lastUserName' =>$lastUserName
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(){
+
+    }
+
+    /**
+     * @Route("/account/create",name="AccountCreate")
+     */
+    public function acountCreateAction(){
+
+    }
+
+
+}
